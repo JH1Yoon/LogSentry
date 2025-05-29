1b64bc2644739736440f38baf1741a90cdfa927e
JH1Yoon <wlgus4589@naver.com>
2025-05-29 14:24:36 +0900

    init commit

---
+++ src/test/java/com/develop/logsentry/LogSentryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.develop.logsentry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogSentryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
